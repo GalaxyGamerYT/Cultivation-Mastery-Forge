9d5f6d59359ed9da91700265ed6d5feb89088ebf
GalaxyGamerYT <OliWootton@Outlook.com>
2025-03-20 19:03:45 +0000

    Made SpiritualMirror a custom Item Class!!!

---
+++ src/main/java/galaxygameryt/cultivation_mastery/item/ModItems.java
@@ -0,0 +1,30 @@
+package galaxygameryt.cultivation_mastery.item;
+
+import galaxygameryt.cultivation_mastery.CultivationMastery;
+import galaxygameryt.cultivation_mastery.item.custom.SpiritualMirrorItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CultivationMastery.MOD_ID);
+
+
+    public static final RegistryObject<Item> SPIRITUAL_MIRROR = ITEMS.register("spiritual_mirror",
+            () -> new SpiritualMirrorItem(new Item.Properties()));
+
+
+    public static final RegistryObject<Item> LOW_SPIRIT_STONE = ITEMS.register("low_spirit_stone",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> MEDIUM_SPIRIT_STONE = ITEMS.register("medium_spirit_stone",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> HIGH_SPIRIT_STONE = ITEMS.register("high_spirit_stone",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
---
+++ src/main/java/galaxygameryt/cultivation_mastery/item/custom/SpiritualMirrorItem.java
@@ -0,0 +1,34 @@
+package galaxygameryt.cultivation_mastery.item.custom;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class SpiritualMirrorItem extends Item {
+
+    public SpiritualMirrorItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pUsedHand);
+        pPlayer.sendSystemMessage(Component.literal("Used Item!!!"));
+        return InteractionResultHolder.success(itemstack);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+
+        pTooltipComponents.add(Component.translatable("item.cultivation_mastery.spiritual_mirror.tooltip"));
+    }
+}
